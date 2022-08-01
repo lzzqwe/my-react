12b1bd80bbf555d70074fb9cce9272c730c5bde4
lzzqwe <352840359@qq.com>
2022-08-01 14:22:31 +0800

    基础课程初步完成

--- src/views/FormSheet/index.jsx
+++ src/views/FormSheet/index.jsx
@@ -1,14 +1,29 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { Button } from "antd";
 class FormSheet extends Component {
   constructor(props) {
     super(props);
     this.state = {
       value: "",
     };
+    this.myRef = React.createRef()
   }
   handleSubmit = (event) => {
     console.log(this.state.value);
     event.preventDefault();
+  };
+  show = () => {
+    const node = this.myRef.current
+    const { history, location, match } = this.props;
+    console.log(location);
+    console.log(match);
+    console.log(history);
+    console.log(node);
+  };
+  back = () => {
+    const { history } = this.props;
+    history.goBack();
   };
   handleChange = (event) => {
     this.setState({
@@ -16,19 +31,31 @@ class FormSheet extends Component {
     });
   };
   render() {
+    const { location } = this.props;
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label htmlFor="">
-          名字
-          <input
-            onChange={this.handleChange}
-            value={this.state.value}
-            type="text"
-          />
-        </label>
-        <input type="submit" value="提交" />
-      </form>
+      <>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="">
+            名字
+            <input
+            ref={this.myRef}
+              onChange={this.handleChange}
+              value={this.state.value}
+              type="text"
+            />
+          </label>
+          <input type="submit" value="提交" />
+        </form>
+        <Button type="primary" onClick={this.show}>
+          显示具体
+        </Button>
+        <p>{location.param?.name ?? "11"}</p>
+        <p>{location?.search ?? "22"}</p>
+        <Button type="primary" onClick={this.back}>
+          返回
+        </Button>
+      </>
     );
   }
 }
-export default FormSheet;
+export default withRouter(FormSheet);
--- src/views/UseState/index.jsx
+++ src/views/UseState/index.jsx
@@ -1,17 +1,91 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect, useRef, useReducer } from "react";
+import { useHistory } from "react-router-dom";
+import { Button } from "antd";
+const initialState = { count: 0 };
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increase":
+      return { count: state.count + 1 };
+    case "decrease":
+      return { count: state.count - 1 };
+    default:
+      throw new Error("wwww");
+  }
+};
 const UseState = () => {
-    const [count,setCount] = useState(0)
-    const handleSet = () => {
-        setCount((state) => {
-         return [1,2,3,3]
-        })
-    }
-    return (
-        <div className="App">
-        <p>{count}</p>
-            <button onClick={handleSet}>增加</button>
-        </div>
-    )
-}
-export default UseState
+  // const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [todo, setTodo] = useState({
+    text: "learn react hook",
+  });
+  const p1 = useRef(null);
+  const history = useHistory();
+  /**
+   * 描述
+   * @date 2022-08-01
+   * @params pathname 路径名字
+   * @params search 字符串查询名字
+   * @params param 提交的参数
+   * @returns {any}
+   */
+  const jump = () => {
+    history.push({
+      pathname: "/formSheet",
+      search: "?some=search-string",
+      param: {
+        name: "lzz",
+      },
+    });
+  };
+  const handleSet = () => {
+    console.log(p1.current);
+    // setCount((state) => {
+    //   return state + 1;
+    // });
+  };
+  const handleTodo = () => {
+    setTodo((state) => {
+      return { ...state, text: "ww" };
+    });
+  };
+
+  useEffect(() => {
+    document.title = `you are click ${state.count} num`;
+  });
+  return (
+    <div className="App">
+      <p ref={p1}>{state.count}</p>
+      <p>{todo.text}</p>
+      <Button onClick={handleSet}>增加</Button>
+      <Button
+       className="ml"
+        type="primary"
+        onClick={() => {
+          dispatch({
+            type: "increase",
+          });
+        }}
+      >
+        增加1
+      </Button>
+      <Button
+        className="ml"
+        type="primary"
+        onClick={() => {
+          dispatch({
+            type: "decrease",
+          });
+        }}
+      >
+        减少1
+      </Button>
+      <Button className="ml" onClick={handleTodo}>
+        改变
+      </Button>
+      <Button className="ml" onClick={jump}>
+        我要跳转去别的页面
+      </Button>
+    </div>
+  );
+};
+export default UseState;
